06a36743f9a49aad7d595f2ed27bd51df51620a8
Rodolphe Roussel <39559477+Rrhodan@users.noreply.github.com>
2023-01-04 21:56:47 +0100

    1er version

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+// This is a comment
+class Progam
+{
+    //this is a comment
+    public static void Main()
+    {
+        var x = 123;
+        Console.WriteLine();
+        Console.WriteLine("hello world!");
+    }
+}
